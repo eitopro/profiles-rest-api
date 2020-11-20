0f1e07307954691b5e5a74b90c7ede609a59c237
eitopro <kim@eito.pro>
2020-11-20 12:03:34 +0200

    django admin and super admin created

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -9,14 +9,14 @@ class UserProfileManager(BaseUserManager):
             raise ValueError('User must have an email address')
 
         email =self.normalize_email(email)
-        user = self.modal(email=email,name=name)
+        user = self.model(email=email,name=name)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
     def create_superuser(self, email, name, password):
-        user = self.create_user.create_user(email, name, password)
+        user = self.create_user(email, name, password)
 
         user.is_superuser =True
         user.is_staff = True
